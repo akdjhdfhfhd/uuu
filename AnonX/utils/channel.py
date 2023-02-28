eebab741c3a514a3cf063aa1291b2c4a9e492a2c
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-28 16:30:20 +0300

    Update channel.py

--- AnonX/utils/channel.py
+++ AnonX/utils/channel.py
@@ -1,23 +1,8 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
+from AnonX import app
 
-from YukkiMusic.plugins.play.filters import command
-from pyrogram import filters
-from pyrogram.types import Message
+from AnonX.utils.database import get_cmode
 
-from config import BANNED_USERS
-from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database import set_cmode
-from YukkiMusic.utils.decorators.admins import AdminActual
 
-### Multi-Lang Commands
 CHANNELPLAY_COMMAND = get_command("CHANNELPLAY_COMMAND")
 
 
